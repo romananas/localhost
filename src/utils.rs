c4f5bede51b14fa1ca11163f0fcf6cb49101c588
romann <romann.belmer@gmail.com>
2025-04-02 13:35:21 +0200

    changed the extention remover from all to .html

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,7 @@
 pub fn remove_extension(filename: &str) -> &str {
-    filename.rsplit_once('.').map(|(name, _)| name).unwrap_or(filename)
+    // filename.rsplit_once('.').map(|(name, _)| name).unwrap_or(filename)
+    match filename.strip_suffix(".html") {
+        Some(v) => v,
+        None => filename,
+    }
 }
