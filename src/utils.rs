3f0ec271edeed1a16c194ba50ae66d956c7252bb
romann <romann.belmer@gmail.com>
2025-04-10 17:36:01 +0200

    added split_get_request() funciton

--- src/utils.rs
+++ src/utils.rs
@@ -8,12 +8,40 @@ pub fn remove_extension(file_path: &str) -> &str {
 
 /// return the file extension of a choosen file if the file does not have an extension the function return None
 pub fn get_file_extention(file_path: &str) -> Option<&str> {
-    let ext = file_path.split(".").collect::<Vec<&str>>();
-    if ext.len() < 2 {
+    let tmp = file_path.split(".").collect::<Vec<&str>>();
+    if tmp.len() < 2 {
         return None;
     } else {
-        return ext.last().copied();
+        let post_point = match tmp.last().copied() {
+            Some(v) => v,
+            None => return None,
+        };
+        let ext = match post_point.split_once("?") {
+            Some(v) => Some(v.0),
+            None => Some(post_point),
+        };
+        return ext;
     }
+}
+/// get a file path as index.php?name=timmy and retrun an option of a (path,request) tuple
+pub fn split_get_request(path: &str) -> Option<(&str,&str)>{
+    let parts = path.split(".").collect::<Vec<&str>>();
+    if parts.len() < 2 {
+        return None;
+    }
+    let ext_and_req = match parts.last().copied() {
+        Some(v) => v,
+        None => return None,
+    };
+    let req_msg = match ext_and_req.split("?").last() {
+        Some(v) => v,
+        None => return None,
+    };
+    let trimmed_path = match path.strip_suffix(format!("?{}",req_msg).as_str()) {
+        Some(v) => v,
+        None => return None,
+    };
+    return Some((trimmed_path,req_msg));
 }
 
 pub fn sanatize_path(file_path: &str) -> &str {
