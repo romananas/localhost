a08001ef24f137e58ebd5348341ffc75434a057f
romann <romann.belmer@gmail.com>
2025-04-14 16:36:11 +0200

    added upload file path variable in opts

--- src/options.rs
+++ src/options.rs
@@ -1,9 +1,7 @@
 #![allow(dead_code)]
 use toml::Value;
 
-use super::args::Args;
 use super::config::Config;
-use super::ip::IPv4;
 
 use std::collections::HashMap;
 
@@ -22,6 +20,7 @@ fn map_to_hashmap(map: toml::map::Map<String,Value>) -> HashMap<String,String>{
 #[derive(Debug,Clone)]
 pub struct Opts {
     pub path: String,
+    pub upload: String,
     pub index: String,
     pub not_found: String,
     pub links: HashMap<String, String>,
@@ -30,29 +29,6 @@ pub struct Opts {
 }
 
 impl Opts {
-    pub fn from_args(a: Args) -> Self {
-        let mut instances: HashMap<String,Vec<u32>> = HashMap::new();
-        for s in a.addr {
-            let ip = IPv4::from(s.as_str()).unwrap();
-            match instances.get_mut(&ip.addr()) {
-                Some(v) => v.push(ip.port),
-                None => { instances.insert(ip.addr(), vec![ip.port]); }
-            }
-        }
-        let mut links: HashMap<String, String> = HashMap::new();
-        let links = match a.links {
-            Some(al) => {
-                for alias in al {
-                    let tmp = alias.split(":").collect::<Vec<&str>>();
-                    links.insert(tmp[0].to_string(), tmp[1].to_string());
-                }
-                links
-            }
-            None => HashMap::new(),
-        };
-        Self { path: a.path, index: a.entry_point, not_found: a.not_found ,links: links, instances: instances, cgi_binds: HashMap::new() }
-    }
-
     pub fn from_config(c: Config) -> Self {
         let mut instances: HashMap<String,Vec<u32>> = HashMap::new();
         for s in c.servers.instance {
@@ -71,12 +47,10 @@ impl Opts {
                 default
             },
         };
-        Self { path: c.path, index: c.servers.index, links: links, not_found: c.servers.not_found, instances: instances, cgi_binds: cgi_binds }
+        Self { path: c.path, index: c.servers.index, links: links, not_found: c.servers.not_found, instances: instances, cgi_binds: cgi_binds, upload: c.uploads_folder }
     }
 
     /// Generate every addresses/port combinations for every instances
-    /// 
-    /// Result = addr:port
     pub fn address_combinations(&self) -> Vec<String> {
         let instances = self.instances.clone();
         instances.iter().flat_map(|(addr,ports)| {
