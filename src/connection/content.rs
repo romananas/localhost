22b343e2ae519f1caa48f70ee8d5347858b36b89
romann <romann.belmer@gmail.com>
2025-04-17 17:01:57 +0200

    content.data is now a Vec<bytes> instead of a String

--- src/connection/content.rs
+++ src/connection/content.rs
@@ -3,11 +3,11 @@
 pub struct Content {
     pub _type: String,
     pub lenght: u32,
-    pub data: String,
+    pub data: Vec<u8>,
 }
 
 impl Content {
-    pub fn new(t: String,lenght: u32, data: String) -> Self {
+    pub fn new(t: String,lenght: u32, data: Vec<u8>) -> Self {
         Self { _type: t, lenght, data }
     }
 }
